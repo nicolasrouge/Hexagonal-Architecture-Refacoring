0add3df65eb20b32667b07383d4d8e66719cd118
Nicolas Rouge <nicolas.rouge777@gmail.com>
2023-12-23 17:29:03 +0000

    move class

--- LiveCoding.Services/BookingService.cs
+++ LiveCoding.Services/BookingService.cs
@@ -2,29 +2,6 @@
 
 namespace LiveCoding.Services
 {
-    public class Bar
-    {
-        private string Name { get; }
-        private int Capacity { get; }
-        private DayOfWeek[] Open { get; }
-
-        public Bar(int capacity, DayOfWeek[] open, string name)
-        {
-            Name = name;
-            Capacity = capacity;
-            Open = open;
-        }
-
-        public bool IsBookable(int maxNumberOfDevs, DateTime bestDate) => 
-            HasEnoughCapacity(maxNumberOfDevs) && IsOpen(bestDate);
-
-        public bool HasEnoughCapacity(int maxNumberOfDevs) => Capacity >= maxNumberOfDevs;
-
-        public bool IsOpen(DateTime bestDate) => Open.Contains(bestDate.DayOfWeek);
-
-        public void BookBar(DateTime dateTime) => Console.WriteLine("Bar booked: " + Name + " at " + dateTime);
-    }
-
     public class BookingService
     {
         private readonly IBarRepository _barRepo;
