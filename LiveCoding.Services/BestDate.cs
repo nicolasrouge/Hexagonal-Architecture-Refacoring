5baf9720c8d67bded2fc1fb0113a84e70f0ef056
Nicolas Rouge <nicolas.rouge777@gmail.com>
2023-12-23 19:35:28 +0000

    move and extract methods and create BestDate static class

---
+++ LiveCoding.Services/BestDate.cs
@@ -0,0 +1,33 @@
+﻿namespace LiveCoding.Services;
+
+public static class BestDate
+{
+    private const double minimumPercentageOfDevs = 0.6;
+
+    public static DevAvailability GetBestDate(List<DevAvailability> devAvailabilities, int totalNUmberOfDevs)
+    {
+        var maxNumberOfDevs = GetMaxNumberOfDevs(devAvailabilities);
+
+        var devAvailability = devAvailabilities.Find(devA => devA.NumberOfDevs == maxNumberOfDevs);
+        //TODO: handle not found
+
+        if (MinimumNumberOfDevsNotAvailable(maxNumberOfDevs, totalNUmberOfDevs))
+        {
+            return null;
+        }
+        else
+        {
+            return devAvailability; ;
+        }
+    }
+
+    private static bool MinimumNumberOfDevsNotAvailable(int maxNumberOfDevs, int totalNumberOfDevs)
+    {
+        return maxNumberOfDevs <= totalNumberOfDevs * minimumPercentageOfDevs;
+    }
+
+    private static int GetMaxNumberOfDevs(IEnumerable<DevAvailability> devAvailabilities)
+    {
+        return devAvailabilities.Max(devAvailability => devAvailability.NumberOfDevs);
+    }
+}
--- LiveCoding.Services/BookingService.cs
+++ LiveCoding.Services/BookingService.cs
@@ -1,4 +1,5 @@
-﻿using LiveCoding.Persistence;
+﻿using System.Net.NetworkInformation;
+using LiveCoding.Persistence;
 
 namespace LiveCoding.Services
 {
@@ -13,6 +14,7 @@ namespace LiveCoding.Services
             Day = day;
         }
     }
+
     public class BookingService
     {
         private readonly IBarRepository _barRepo;
@@ -38,51 +40,44 @@ namespace LiveCoding.Services
             var devs = _devRepo.Get().ToList();
             var boats = _boatRepo.Get();
 
-            var devAvailabilities = new List<DevAvailability>();
+            var devAvailabilities = GetDevAvailabilities(devs);
 
-            foreach (var date in devs.SelectMany(devData => devData.OnSite))
-            {
-                var devAvailability = devAvailabilities.FirstOrDefault(devAvailability => devAvailability.Day == date);
-                if (devAvailability != null)
-                {
-                    devAvailability.NumberOfDevs++;
-                }
-                else
-                {
-                    devAvailabilities.Add(new DevAvailability(date, 1));
-                }
-            }
+            var bestDate = BestDate.GetBestDate(devAvailabilities, devs.Count);
 
-            var maxNumberOfDevs = devAvailabilities.Max(devAvailability => devAvailability.NumberOfDevs);
-
-            if (maxNumberOfDevs <= devs.Count() * 0.6)
-            {
-                return false;
-            }
-
-            var bestDate = devAvailabilities.Find(devA => devA.NumberOfDevs == maxNumberOfDevs).Day;
-            //TODO: handle not found
+            if (bestDate is null) return false;
 
             foreach (var boatData in boats)
             {
                 var bar = new Bar(boatData.MaxPeople, Enum.GetValues<DayOfWeek>(), boatData.Name);
-                if (!bar.IsBookable(maxNumberOfDevs, bestDate)) continue;
-                bar.BookBar(bestDate);
-                _bookingRepository.Save(new BookingData() { Bar = new BarData(boatData.Name, boatData.MaxPeople, Enum.GetValues<DayOfWeek>() ), Date = bestDate });
+                if (!bar.IsBookable(bestDate.NumberOfDevs, bestDate.Day)) continue;
+                bar.BookBar(bestDate.Day);
+                _bookingRepository.Save(new BookingData() { Bar = new BarData(boatData.Name, boatData.MaxPeople, Enum.GetValues<DayOfWeek>()), Date = bestDate.Day });
                 return true;
             }
 
             foreach (var barData in bars)
             {
                 var bar = new Bar(barData.Capacity, barData.Open, barData.Name);
-                if (!bar.IsBookable(maxNumberOfDevs, bestDate)) continue;
-                bar.BookBar(bestDate);
-                _bookingRepository.Save(new BookingData() { Bar = barData, Date = bestDate });
+                if (!bar.IsBookable(bestDate.NumberOfDevs, bestDate.Day)) continue;
+                bar.BookBar(bestDate.Day);
+                _bookingRepository.Save(new BookingData() { Bar = barData, Date = bestDate.Day });
                 return true;
             }
 
             return false;
         }
 
+        private static List<DevAvailability> GetDevAvailabilities(IEnumerable<DevData> devs)
+        {
+            var devAvailabilities = new List<DevAvailability>();
+            foreach (var date in devs.SelectMany(devData => devData.OnSite))
+            {
+                var devAvailability = devAvailabilities.FirstOrDefault(devAvailability => devAvailability.Day == date);
+                if (devAvailability != null) devAvailability.NumberOfDevs++;
+                else devAvailabilities.Add(new DevAvailability(date, 1));
+            }
+
+            return devAvailabilities;
+        }
     }
 }
